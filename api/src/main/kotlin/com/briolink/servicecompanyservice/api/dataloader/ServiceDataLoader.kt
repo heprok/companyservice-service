7d60347bf02c759c763099fc002cba470e202ab3
Oleg Kravchuck <oleg@briolink.com>
2021-10-28 03:55:16 +0300

    rename databases

--- api/src/main/kotlin/com/briolink/servicecompanyservice/api/dataloader/ServiceDataLoader.kt
+++ api/src/main/kotlin/com/briolink/servicecompanyservice/api/dataloader/ServiceDataLoader.kt
@@ -4,9 +4,9 @@ import com.briolink.servicecompanyservice.api.service.ServiceCompanyService
 import com.briolink.servicecompanyservice.common.jpa.read.repository.CompanyReadRepository
 import com.briolink.servicecompanyservice.common.jpa.write.repository.ServiceWriteRepository
 import org.springframework.core.annotation.Order
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.stereotype.Component
 import java.net.URL
-import java.time.Instant
 import kotlin.random.Random
 
 @Component
@@ -38,15 +38,19 @@ class ServiceDataLoader(
         ) {
             val companyList = companyReadRepository.findAll()
             for (i in 1..COUNT_SERVICE) {
-                serviceCompanyService.create(
-                        companyId = companyList.random().id,
-                        logo = URL("https://placeimg.com/640/640/tech"),
-                        created = Instant.from(randomDate(2010, 2021)),
-                        name = listName[(i % listName.count())],
-                        price = Random.nextDouble(0.0, 6000000.0),
-                        description = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-                        logoTempKey = null,
-                )
+                try {
+                    serviceCompanyService.create(
+                            companyId = companyList.random().id,
+                            logo = URL("https://placeimg.com/640/640/tech"),
+                            name = listName[(i % listName.count())],
+                            price = Random.nextDouble(0.0, 6000000.0),
+                            description = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+                            logoTempKey = null,
+                    )
+                } catch (e: DataIntegrityViolationException) {
+
+                }
+
             }
         }
     }
