0e5c824d45ef371da49a972c44d10a7961b77f8a
Oleg Kravchuck <ok@techno-les.com>
2021-11-05 18:10:19 +0200

    jdbc driver for postgresql

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -31,15 +31,15 @@ dependencies {
     // Liquibase
     implementation("org.liquibase:liquibase-core:${Versions.LIQUIBASE_CORE}")
 
-    // MariaDB JDBC Driver
-    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-
     // Netflix DGS
     implementation(
             platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release"),
     )
     implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
     implementation("com.netflix.graphql.dgs:graphql-dgs-extended-scalars")
+
+    // postgtrsql JDBC Driver
+    runtimeOnly("org.postgresql:postgresql:${Versions.POSTGRESQL}")
 
     // kotlin-logging
     implementation("io.github.microutils:kotlin-logging-jvm:${Versions.KOTLIN_LOGGING_JVM}")
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -19,9 +19,6 @@ dependencies {
     kapt("org.springframework.boot:spring-boot-autoconfigure-processor:${Versions.SPRING_BOOT}")
     kapt("org.springframework.boot:spring-boot-configuration-processor:${Versions.SPRING_BOOT}")
 
-    // postgresql
-    implementation("org.postgresql:postgresql:${Versions.POSTGRESQL}")
-
     // FasterXML
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
