2b46515a9509d7cafb72e07a6741ecbb4ac02f3d
Oleg Kravchuck <ok@techno-les.com>
2021-12-07 02:36:11 +0200

    Add method to deleete all user_permission_role

--- api/src/main/kotlin/com/briolink/servicecompanyservice/api/rest/ApiRest.kt
+++ api/src/main/kotlin/com/briolink/servicecompanyservice/api/rest/ApiRest.kt
@@ -4,19 +4,24 @@ import com.briolink.event.publisher.EventPublisher
 import com.briolink.servicecompanyservice.api.dataloader.ServiceDataLoader
 import com.briolink.servicecompanyservice.common.domain.v1_0.Statistic
 import com.briolink.servicecompanyservice.common.event.v1_0.CompanyServiceStatisticRefreshEvent
+import com.briolink.servicecompanyservice.common.jpa.read.repository.UserPermissionRoleReadRepository
 import org.springframework.http.ResponseEntity
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1")
+@Transactional
 class ApiRest(
     private val eventPublisher: EventPublisher,
-    private val serviceDataLoader: ServiceDataLoader
+    private val serviceDataLoader: ServiceDataLoader,
+    private val userPermissionRoleReadRepository: UserPermissionRoleReadRepository,
 ) {
     @GetMapping("/statistic/refresh")
     fun refreshStatistic(): ResponseEntity<Int> {
+        userPermissionRoleReadRepository.deleteAll()
         eventPublisher.publishAsync(
             CompanyServiceStatisticRefreshEvent(Statistic(null)),
         )
