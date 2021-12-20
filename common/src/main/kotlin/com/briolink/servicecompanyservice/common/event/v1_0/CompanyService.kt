bfdfeac20b7844f3b57a5d96c1eac920ac489d5b
Oleg Kravchuck <ok@techno-les.com>
2021-12-20 16:29:16 +0200

    BNM-467 send statistic to search service

--- common/src/main/kotlin/com/briolink/servicecompanyservice/common/event/v1_0/CompanyService.kt
+++ common/src/main/kotlin/com/briolink/servicecompanyservice/common/event/v1_0/CompanyService.kt
@@ -4,9 +4,11 @@ import com.briolink.event.Event
 import com.briolink.servicecompanyservice.common.domain.v1_0.CompanyService
 import com.briolink.servicecompanyservice.common.domain.v1_0.CompanyServiceDeletedData
 import com.briolink.servicecompanyservice.common.domain.v1_0.CompanyServiceHideData
+import com.briolink.servicecompanyservice.common.domain.v1_0.CompanyServiceStatistic
 import com.briolink.servicecompanyservice.common.domain.v1_0.Domain
 
 data class CompanyServiceCreatedEvent(override val data: CompanyService) : Event<Domain>("1.0")
 data class CompanyServiceUpdatedEvent(override val data: CompanyService) : Event<Domain>("1.0")
 data class CompanyServiceDeletedEvent(override val data: CompanyServiceDeletedData) : Event<Domain>("1.0")
 data class CompanyServiceHideEvent(override val data: CompanyServiceHideData) : Event<Domain>("1.0")
+data class CompanyServiceStatisticEvent(override val data: CompanyServiceStatistic) : Event<Domain>("1.0")
