eb5b5937d31dc061de72b68315e2943ed62fe809
Oleg <Kravchuk>
2022-03-28 03:02:28 +0300

    Add anonymous access

--- api/src/main/kotlin/com/briolink/servicecompanyservice/api/graphql/query/ConnectionQuery.kt
+++ api/src/main/kotlin/com/briolink/servicecompanyservice/api/graphql/query/ConnectionQuery.kt
@@ -15,7 +15,6 @@ import com.briolink.servicecompanyservice.common.util.StringUtil
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsQuery
 import com.netflix.graphql.dgs.InputArgument
-import org.springframework.security.access.prepost.PreAuthorize
 import java.util.UUID
 
 @DgsComponent
@@ -24,24 +23,23 @@ class ConnectionQuery(
     private val serviceCompanyService: ServiceCompanyService
 ) {
     @DgsQuery
-    @PreAuthorize("isAuthenticated()")
     fun getConnections(
-        @InputArgument("serviceId") serviceId: String,
-        @InputArgument("companyId") companyId: String,
-        @InputArgument("filter") filter: ConnectionFilter,
-        @InputArgument("sort") sort: ConnectionSort,
-        @InputArgument("limit") limit: Int,
-        @InputArgument("offset") offset: Int,
+        @InputArgument serviceId: String,
+        @InputArgument companyId: String,
+        @InputArgument filter: ConnectionFilter,
+        @InputArgument sort: ConnectionSort,
+        @InputArgument limit: Int?,
+        @InputArgument offset: Int?,
     ): ConnectionList {
         return if (connectionService.existsConnectionByService(serviceId = UUID.fromString(serviceId))
         ) {
-            val securityFilter = if (
+            val securityFilter = if (!SecurityUtil.isGuest &&
                 serviceCompanyService.isHavePermission(
-                    companyId = UUID.fromString(companyId),
-                    userId = SecurityUtil.currentUserAccountId,
-                    permissionRight = PermissionRightEnum.IsCanEditProject,
-                    serviceId = UUID.fromString(serviceId),
-                )
+                        companyId = UUID.fromString(companyId),
+                        userId = SecurityUtil.currentUserAccountId,
+                        permissionRight = PermissionRightEnum.IsCanEditProject,
+                        serviceId = UUID.fromString(serviceId),
+                    )
             ) filter else filter.copy(isHidden = false)
 
             val result = connectionService.findAll(
@@ -49,35 +47,33 @@ class ConnectionQuery(
                 companyId = UUID.fromString(companyId),
                 sort = sort,
                 filter = securityFilter,
-                limit = limit,
-                offset = offset,
+                limit = limit ?: 10,
+                offset = offset ?: 0,
             )
             ConnectionList(
                 items = result.map { Connection.fromEntity(it) },
                 totalItems = connectionService.count(
                     serviceId = UUID.fromString(serviceId),
                     companyId = UUID.fromString(companyId),
-                    filter = filter,
+                    filter = securityFilter,
                 ).toInt(),
             )
         } else ConnectionList(items = listOf(), totalItems = -1)
     }
 
     @DgsQuery
-    @PreAuthorize("isAuthenticated()")
     fun getConnectionsCount(
-        @InputArgument("serviceId") serviceId: String,
-        @InputArgument("companyId") companyId: String,
-        @InputArgument("filter") filter: ConnectionFilter
+        @InputArgument serviceId: String,
+        @InputArgument companyId: String,
+        @InputArgument filter: ConnectionFilter
     ): Int =
         connectionService.count(serviceId = UUID.fromString(serviceId), companyId = UUID.fromString(companyId), filter = filter).toInt()
 
     @DgsQuery
-    @PreAuthorize("isAuthenticated()")
     fun getConnectionCollaborators(
-        @InputArgument("serviceId") serviceId: String,
-        @InputArgument("companyId") companyId: String,
-        @InputArgument("query") query: String,
+        @InputArgument serviceId: String,
+        @InputArgument companyId: String,
+        @InputArgument query: String,
     ): List<Collaborator> = connectionService.getCollaboratorsUsedForCompany(
         serviceId = UUID.fromString(serviceId),
         companyId = UUID.fromString(companyId),
@@ -85,11 +81,10 @@ class ConnectionQuery(
     )
 
     @DgsQuery
-    @PreAuthorize("isAuthenticated()")
     fun getConnectionIndustries(
-        @InputArgument("query") query: String,
-        @InputArgument("serviceId") serviceId: String,
-        @InputArgument("companyId") companyId: String
+        @InputArgument query: String,
+        @InputArgument serviceId: String,
+        @InputArgument companyId: String
     ): List<Industry> =
         connectionService.getIndustriesInConnectionFromCompany(
             serviceId = UUID.fromString(serviceId),
